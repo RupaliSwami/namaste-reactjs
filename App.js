adba9d9b79c2a69d28e51bb8a078bb0e04a53338
rupaliswami <swamirupali27@gmail.com>
2025-08-05 21:40:27 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,39 @@
+// const heading = React.createElement('h1', {id: 'heading'}, "Hello World from Namaste ReactJS");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(heading);
+
+/****
+ * 
+ * <div id="parent">
+ * <div id="child1">
+ *  <h1>This is a h1 tag</h1>
+ *  <h2>This is a h2 tag</h2> 
+ * </div>
+ * <div id="child2">
+ *  <h1>This is h2 tag.</h1>
+ *  <h2>This is a h2 tag.</h2>
+ * </div>
+ *
+ * </div>
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+const heading =  React.createElement('div', {id: 'parent'}, 
+    [ React.createElement('div', {id: 'child1'}, [
+        React.createElement('h1', {}, "This is h1 tag."),
+        React.createElement('h2', {}, "This is h2 tag.")
+    ]), 
+      React.createElement('div', {id: 'child2'}, [
+        React.createElement('h1', {}, "This is h1 tag."),
+        React.createElement('h2', {}, "This is h2 tag.")
+      ])
+    ] 
+    )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
